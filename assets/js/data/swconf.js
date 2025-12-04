29ab2d12b0cc463cc7a3e445a2b553ac62ddbd69
Das Alpha <das.alpha@protonmail.com>
2025-12-04 14:31:46 +0100

    Build

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1764854544',resources: [
+    cacheName: 'chirpy-1764855104',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
